e86e0bf8d20d8f47b4a5825a8fc6f38133b2bd5e
Moazam05 <salmanmoazam08@gmail.com>
2024-10-17 15:42:09 +0500

    register

---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+// Custom Imports
+const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  // Remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.register = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+  });
+
+  createSendToken(newUser, 201, res);
+});
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Auth");
-});
+router.post("/register", authController.register);
 
 module.exports = router;
