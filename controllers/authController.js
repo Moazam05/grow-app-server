3560d565ba9f6a8224a21e8a95e1dff094cfdd00
Moazam05 <salmanmoazam08@gmail.com>
2024-10-17 16:26:30 +0500

    login api

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 // Custom Imports
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -33,4 +34,27 @@ exports.register = catchAsync(async (req, res, next) => {
   });
 
   createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password", 400));
+  }
+
+  // 2) Check if user exists && password is exist
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+
+  const correct = await user.correctPassword(password, user.password);
+  if (!correct) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+
+  // 3) If everything ok, send token to client
+  createSendToken(user, 200, res);
 });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,8 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
+// todo: AUTH ROUTES
 router.post("/register", authController.register);
+router.post("/login", authController.login);
 
 module.exports = router;
