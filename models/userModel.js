5b6f3bc8514bf12658c8c1ca0fb8da15df443c35
Moazam05 <salmanmoazam08@gmail.com>
2024-10-17 16:14:30 +0500

    schema update

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,82 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      maxLength: 50,
       minlength: 3,
       required: [true, "Please tell us your name!"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: [true, "Please provide your email"],
+      validate: [validator.isEmail, "Please provide a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 6,
+      maxLength: 20,
+    },
+    login_pin: {
+      type: String,
+      minlength: 4,
+      maxLength: 4,
+    },
+    phone_number: {
+      type: String,
+      validate: [
+        validator.isMobilePhone,
+        "Please provide a valid phone number",
+      ],
+    },
+    address: {
+      type: String,
+    },
+    date_of_birth: {
+      type: Date,
+    },
+    email_verified: {
+      type: Boolean,
+      default: false,
+    },
+    phone_verified: {
+      type: Boolean,
+      default: false,
+    },
+    bank_amount: {
+      type: Number,
+      default: 0,
     },
   },
   {
     timestamps: true,
   }
 );
+
+// Password Hashing
+userSchema.pre("save", async function (next) {
+  // Only hash the password if it is new or has been modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+
+// instance method
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = new mongoose.model("User", userSchema);
 module.exports = User;
